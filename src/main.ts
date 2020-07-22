5b7278bc2188feb00c753cdc2ab7904481721518
andrew-polk <andrew_polk@sil.org>
2020-07-22 10:48:34 -0700

    Use a single const to control the environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { BookInstanceIdBackfiller } from "./BookInstanceIdBackfiller";
 import PostgresConnection from "./connections/PostgresConnection";
+import { Environment } from "./Environment";
+
+// Controls which parse database and postgresql schema(s) we connect to...
+export const environment: Environment = Environment.Dev;
 
 main();
 
 async function main(): Promise<void> {
+    console.log();
+    console.log(
+        "########################################################################"
+    );
+    console.log(
+        `Environment: ${
+            environment === Environment.Prod
+                ? "Production"
+                : environment === Environment.Dev
+                ? "Development"
+                : "Local"
+        }`
+    );
+    console.log(
+        "########################################################################"
+    );
+    console.log();
+
     const backfiller = new BookInstanceIdBackfiller();
     await backfiller.backfill();
 
